6e9ff14a8dd1a6fedd7afaea4ec5d2eb28bb98b8
Kolyall <kolyall@tut.by>
2018-11-09 14:25:30 +0300

    v 1.1.2

--- picasso-transformations/src/main/java/com/utils/picasso/SimpleTarget.java
+++ picasso-transformations/src/main/java/com/utils/picasso/SimpleTarget.java
@@ -1,7 +1,9 @@
 package com.utils.picasso;
 
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 
+import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
 /**
@@ -11,7 +13,12 @@ import com.squareup.picasso.Target;
 public abstract class SimpleTarget implements Target {
 
     @Override
-    public void onBitmapFailed(Drawable errorDrawable) {
+    public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+
+    }
+
+    @Override
+    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
 
     }
 
